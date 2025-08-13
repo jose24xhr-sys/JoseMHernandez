34f6c43977ff3dfe5d3973b9316b0da52bbe5f9e
jose24xhr-sys <jose24xhr@gmail.com>
2025-08-13 15:26:21 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -240,7 +240,7 @@ export default function App() {
        {/* elimina el viejo <div ref={heroRef} ... /> */}
 
 {inHero && (
-  <div className="fixed bottom-6 left-6 z-50" aria-label="Theme toggle floating">
+  <div className="fixed bottom-4 left-4 z-50" aria-label="Theme toggle floating">
     <ThemeToggle checked={darkMode} onToggle={() => setDarkMode(v => !v)} />
   </div>
 )}
