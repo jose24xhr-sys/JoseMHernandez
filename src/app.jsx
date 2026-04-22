1d38a7b542b67365a77428c8ab64ed31bebad064
jose24xhr-sys <jose24xhr@gmail.com>
2026-04-22 03:50:02 +0000

    test1

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Routemize from "./pages/Routemize";
+import BehavioralTest from "./pages/BehavioralTest";
 
 export default function App() {
   return (
@@ -8,6 +9,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/routemize" element={<Routemize />} />
+        <Route path="/behavioraltest" element={<BehavioralTest />} />
+
       </Routes>
     </BrowserRouter>
   );
