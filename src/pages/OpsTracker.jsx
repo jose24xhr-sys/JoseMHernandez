d3c7ea56c6579c297c076bbec7151154bade397f
jose24xhr-sys <jose24xhr@gmail.com>
2026-06-03 13:55:02 +0000

    update ops tracker2

--- src/pages/OpsTracker.jsx
+++ src/pages/OpsTracker.jsx
@@ -2,7 +2,8 @@ export default function OpsTracker() {
   return (
     <iframe
       src="/ops-tracker.html"
-      style={{ width: '100%', height: '100vh', border: 'none' }}
+      style={{ width: "100%", height: "100vh", border: "none" }}
+      title="Ops Tracker"
     />
-  )
+  );
 }
