6df5e996da9d1a43c55169e0520a629fb910ec52
jose24xhr-sys <jose24xhr@gmail.com>
2026-06-03 14:21:52 +0000

    update ops tracker6

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Routemize from "./pages/Routemize";
 import OpsTracker from './pages/OpsTracker';
 
 export default function App() {
@@ -8,7 +7,6 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/routemize" element={<Routemize />} />
         <Route path="/ops-tracker" element={<OpsTracker />} />
       </Routes>
     </BrowserRouter>
