356ac3111b183343cc1d0b44cbd8f9555ff297f0
jose24xhr-sys <jose24xhr@gmail.com>
2026-06-03 14:14:39 +0000

    update ops tracker5

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Routemize from "./pages/Routemize";
-import OpsTracker from './pages/OpsTracker'
+import OpsTracker from './pages/OpsTracker';
 
-
-export default function App() {git add .
-
-git commit -m "update ops tracker3"
-
-git push origin main
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/routemize" element={<Routemize />} />
         <Route path="/ops-tracker" element={<OpsTracker />} />
-
       </Routes>
     </BrowserRouter>
   );
